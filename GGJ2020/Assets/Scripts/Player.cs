ee8b2886713b2e2f79db4a909c45a46619db6c86
Fabian Schroth <fabian.schroth@unitybox.de>
2020-01-31 23:00:46 +0100

    Added Player & Weapon Scripts

---
+++ GGJ2020/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Members / Properties / Constants
+
+    int m_RemainingHealth;
+    int m_MaxHealth;
+
+    public Weapon CurrentWeapon { get; set; }
+
+    #endregion
+
+    #region Functions
+
+    /// <summary>
+    /// Called when the player got hit by an enemy
+    /// </summary>
+    /// <param name="_amount">The amount of damage to take</param>
+    public void TakeDamage(int _amount)
+    {
+        m_RemainingHealth -= _amount;
+
+        //TODO: Update GUI
+
+        if (m_RemainingHealth <= 0)
+        {
+            // TODO: Game Over
+        }
+    }
+
+    #endregion
+}
---
+++ GGJ2020/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+
+    #region Members / Properties / Constants
+
+    private string Name { get; set; }
+    private int Damage { get; set; }
+    private int AttackSpeed { get; set; }
+
+    #endregion
+}
