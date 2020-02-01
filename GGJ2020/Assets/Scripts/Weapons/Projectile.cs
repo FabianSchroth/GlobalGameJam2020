3489cc4ae8c5dfe92f9e3f872ee97d5d055b03ab
Bennipow <bg123@gmx.de>
2020-02-01 17:04:18 +0100

    AddedSomeScripts

---
+++ GGJ2020/Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_Speed;    
+
+    protected abstract void HitSomething();
+}
