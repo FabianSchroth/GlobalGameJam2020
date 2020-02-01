5d5be2dce5cbeb9495e099d659f3fa2a494351cf
Fabian Schroth <fabian.schroth@unitybox.de>
2020-02-01 13:15:24 +0100

    Fixed Player Merge

--- GGJ2020/Assets/Scripts/Player.cs
+++ GGJ2020/Assets/Scripts/Player.cs
@@ -17,41 +17,15 @@ public class Player : MonoBehaviour
 
     public Weapon CurrentWeapon { get; set; }
 
-    private CharacterController m_CC;
+    private CharacterController m_CharacterController;
 
     private PlayerControls m_Controls;
 
     private Vector2 moveInput;
+
     #endregion
 
     #region Functions
-
-    private void Awake()
-    {
-        m_CC = GetComponent<CharacterController>();
-
-        m_Controls = new PlayerControls();
-
-        m_Controls.PlayerMaps.Movement.performed += _ => moveInput = m_Controls.PlayerMaps.Movement.ReadValue<Vector2>();
-        m_Controls.PlayerMaps.Movement.canceled += _ => moveInput = m_Controls.PlayerMaps.Movement.ReadValue<Vector2>();
-
-        m_Controls.PlayerMaps.Shoot.performed += _ => Shoot();
-    }
-
-    private void OnEnable()
-    {
-        m_Controls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        m_Controls.Disable();
-    }
-
-    private void Update()
-    {
-        Movement();
-    }
 
     private void Shoot()
     {
@@ -63,7 +37,7 @@ public class Player : MonoBehaviour
         float hor = moveInput.x;
         float vert = moveInput.y;
 
-        m_CC.Move(new Vector3(hor,0, vert));
+        m_CharacterController.Move(new Vector3(hor,0, vert));
     }
 
     /// <summary>
@@ -92,11 +66,35 @@ public class Player : MonoBehaviour
             Destroy(this.gameObject);
         else
             m_Instance = this;
+
+        m_CharacterController = GetComponent<CharacterController>();
+
+        m_Controls = new PlayerControls();
+
+        m_Controls.PlayerMaps.Movement.performed += _ => moveInput = m_Controls.PlayerMaps.Movement.ReadValue<Vector2>();
+        m_Controls.PlayerMaps.Movement.canceled += _ => moveInput = m_Controls.PlayerMaps.Movement.ReadValue<Vector2>();
+
+        m_Controls.PlayerMaps.Shoot.performed += _ => Shoot();
     }
 
     private void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void OnEnable()
+    {
+        m_Controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_Controls.Disable();
     }
 
     #endregion
