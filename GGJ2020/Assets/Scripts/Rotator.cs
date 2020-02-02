47c35ad36d8f5ae865d031693289bd5d94602cae
Fabian Schroth <fabian.schroth@unitybox.de>
2020-02-02 04:31:35 +0100

    Rotator Script + Room Prefab Fix

---
+++ GGJ2020/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private int m_RotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up, Time.deltaTime * m_RotationSpeed);
+    }
+}
