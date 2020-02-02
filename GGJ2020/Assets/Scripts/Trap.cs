17d91cde29ec89ab22b864293659df02e44978bc
Bennipow <bg123@gmx.de>
2020-02-02 14:38:50 +0100

    AddedLightsand traps

---
+++ GGJ2020/Assets/Scripts/Trap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource),typeof(Collider))]
+public class Trap : MonoBehaviour
+{
+    [SerializeField]
+    private int m_Damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            player.TakeDamage(m_Damage);
+        }
+        Enemy enemy = other.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            enemy.TakeDamage(m_Damage);
+        }
+    }
+}
